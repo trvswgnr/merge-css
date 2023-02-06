87164ad014df102867aea33e2557c39a37b9a0b7
Travis Wagner <travis.wagner@umusic.com>
2023-02-05 21:30:57 -0500

    convert to ts

---
+++ src/cli.ts
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+import { readFileSync, existsSync } from 'fs';
+import { combineUniqueCss, IOptions } from './combine-unique-css';
+
+const args = process.argv.slice(2);
+const options = parseCliArgs(args);
+
+combineUniqueCss(options);
+
+/**
+ * Parse the command line arguments and return an object with the arguments.
+ * @param args The command line arguments, starting with the first argument after the script name.
+ * @returns An object from the parsed arguments.
+ */
+function parseCliArgs(args: string[]): IOptions {
+    if (args.length < 2) {
+        showHelp();
+        process.exit(1);
+    }
+
+    if (args[0] === '-h' || args[0] === '--help') {
+        showHelp();
+        process.exit(0);
+    }
+
+    const file1 = args[0];
+    const file2 = args[1];
+    let output = '';
+    let parent = '';
+
+    for (let i = 2; i < args.length; i++) {
+        const arg = args[i];
+        switch (arg) {
+            case '-o' || '--output':
+                output = args[i + 1];
+                i++;
+                break;
+            case '-p' || '--parent':
+                parent = args[i + 1];
+                i++;
+                break;
+            default:
+                console.error(`Error: Unknown argument ${arg}`);
+                showHelp();
+                process.exit(1);
+        }
+    }
+
+    const files = [file1, file2, output];
+    files.forEach((file, i) => {
+        if (file && !file.endsWith('.css')) {
+            files[i] = file + '.css';
+        }
+
+        // make sure the file exists, unless it's the output file
+        if (i < 2 && !existsSync(files[i])) {
+            console.error(`Error: File ${files[i]} does not exist.`);
+            process.exit(1);
+        }
+    });
+
+    const css1 = readFileSync(file1, 'utf8');
+    const css2 = readFileSync(file2, 'utf8');
+
+    const options: IOptions = {
+        css1,
+        css2,
+    };
+
+    if (output) {
+        options.output = output;
+    }
+
+    if (parent) {
+        options.parent = parent;
+    }
+
+    return options;
+}
+
+function showHelp() {
+    let help = `
+        > Usage:
+        >   combine-unique-css <file1> <file2> [options]
+        >
+        > Options:
+        >   -o, --output <file>        The output file to write the combined CSS to.
+        >   -p, --parent <selector>    The parent selector to wrap the combined CSS in.
+        >   -h, --help                 Show this help message.
+    `;
+
+    help = help.trim().split('\n').map((line) => line.trim().replace(/^>\s?/, '')).join('\n');
+    console.info(help);
+}
---
+++ src/combine-unique-css.ts
@@ -0,0 +1,146 @@
+import { writeFileSync } from 'fs';
+import postcss, { AcceptedPlugin, Root, AtRule, Rule } from 'postcss';
+import uniqueCss from './unique-css';
+import { format } from 'prettier';
+import nested from 'postcss-nested';
+import prefixer from 'postcss-prefix-selector';
+
+/**
+ * Combine two CSS files, keeping only the unique CSS from each file,
+ * sort the CSS, wrap it in a parent, then write it to a file.
+ * @param {IOptions} options The options to use.
+ * @returns {string} The combined CSS.
+ */
+export function combineUniqueCss(options: IOptions): string {
+    const v1 = options.css1;
+    const v1Parsed = postcss.parse(v1);
+
+    // remove the parent class from all selectors
+    if (options.parent) {
+        v1Parsed.walkRules((rule) => {
+            const regex = new RegExp(`${options.parent}\\s?`, 'g');
+            rule.selector = rule.selector.replace(regex, '');
+        });
+    }
+
+    // convert the CSS back to a string
+    const v1NoParent = v1Parsed.toString();
+
+    const v2 = options.css2;
+
+    // get the unique CSS from v1 compared to v2
+    const v1Unique = uniqueCss(v1NoParent, v2);
+
+    // get the unique CSS from v2 compared to v1
+    const v2Unique = uniqueCss(v2, v1NoParent);
+
+    const combined = v1Unique + '\n' + v2Unique;
+
+    const sorted = sortCss(combined);
+
+    // format the CSS with prettier
+    const formatted = format(sorted.toString(), { parser: 'css' });
+
+    let plugins: AcceptedPlugin[] = [];
+
+    if (options.parent) {
+        plugins = [
+            nested,
+            // @ts-ignore
+            prefixer({
+                prefix: options.parent,
+                exclude: [options.parent],
+            }),
+        ];
+    }
+
+
+    // wrap all the CSS in a parent class
+    const result = postcss(plugins).process(formatted, {
+        from: undefined,
+    });
+
+
+    // write the CSS to a file, or the console if no output file is specified
+    if (options.output) {
+        writeFileSync(options.output, result.css);
+        console.log(`Successfully combined CSS into ${options.output}`);
+    } else {
+        console.log(result.css);
+    }
+
+    return result.css;
+}
+
+/**
+ * Sorts the CSS like this:
+ *  1. :root selectors
+ *  2. * selectors
+ *  3. html and body selectors
+ *  4. pseudo selectors
+ *  5. tag selectors
+ *  6. class selectors
+ *  7. at-rules
+ *
+ * @param {string} css The CSS to sort.
+ */
+function sortCss(css: string): Root {
+    const rootSelectors: Rule[] = [];
+    const starSelectors: Rule[] = [];
+    const baseSelectors: Rule[] = [];
+    const pseudo: Rule[] = [];
+    const tags: Rule[] = [];
+    const classes: Rule[] = [];
+    const atrules: AtRule[] = [];
+
+    const combinedParsed = postcss.parse(css);
+
+    combinedParsed.walkRules((rule) => {
+        // skip rules inside of @media
+        if (rule.parent?.type === 'atrule') {
+            return;
+        }
+
+        if (rule.selector.startsWith('*')) {
+            starSelectors.push(rule);
+        } else if (rule.selector.startsWith(':root')) {
+            rootSelectors.push(rule);
+        } else if (rule.selector.startsWith('html') || rule.selector.startsWith('body')) {
+            baseSelectors.push(rule);
+        } else if (rule.selector.startsWith(':')) {
+            pseudo.push(rule);
+        } else if (rule.selector.startsWith('.')) {
+            classes.push(rule);
+        } else {
+            tags.push(rule);
+        }
+    });
+
+    // put the @ rules at the end
+    combinedParsed.walkAtRules((atrule) => {
+        atrules.push(atrule);
+    });
+
+    // combine the CSS back together
+    const root = postcss.root();
+    root.append(rootSelectors);
+    root.append(starSelectors);
+    root.append(baseSelectors);
+    root.append(pseudo);
+    root.append(tags);
+    root.append(classes);
+    root.append(atrules);
+
+    return root;
+}
+
+export interface IOptions {
+    /** The first CSS file contents. */
+    css1: string;
+    /** The second CSS file contents. */
+    css2: string;
+    /** The output file to write the combined CSS to. */
+    output?: string;
+    /** The parent selector to wrap the combined CSS in. */
+    parent?: string;
+}
