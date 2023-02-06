2a3944b20c1bf07ef299c91d8b6deb3fae6f68f2
Travis Wagner <travis.wagner@umusic.com>
2023-02-05 19:29:48 -0500

    add cli file

---
+++ cli.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+// @ts-check
+import { readFileSync, existsSync } from 'fs';
+import * as CombineCss from './combine-unique-css.js';
+
+const args = process.argv.slice(2);
+const options = parseCliArgs(args);
+
+CombineCss.combine(options);
+
+/**
+ * Parse the command line arguments and return an object with the arguments.
+ * @param {string[]} args The command line arguments, starting with the first argument after the script name.
+ * @returns {CombineCss.IOptions} An object from the parsed arguments.
+ */
+function parseCliArgs(args) {
+    if (args.length < 2) {
+        showHelp();
+        process.exit(1);
+    }
+
+    if (args[0] === '-h' || args[0] === '--help') {
+        showHelp();
+        process.exit(0);
+    }
+
+    const file1 = args[0];
+    const file2 = args[1];
+    let output = '';
+    let parent = '';
+
+    for (let i = 2; i < args.length; i++) {
+        const arg = args[i];
+        switch (arg) {
+            case '-o' || '--output':
+                output = args[i + 1];
+                i++;
+                break;
+            case '-p' || '--parent':
+                parent = args[i + 1];
+                i++;
+                break;
+            default:
+                console.error(`Error: Unknown argument ${arg}`);
+                showHelp();
+                process.exit(1);
+        }
+    }
+
+    const files = [file1, file2, output];
+    files.forEach((file, i) => {
+        if (file && !file.endsWith('.css')) {
+            files[i] = file + '.css';
+        }
+
+        // make sure the file exists, unless it's the output file
+        if (i < 2 && !existsSync(files[i])) {
+            console.error(`Error: File ${files[i]} does not exist.`);
+            process.exit(1);
+        }
+    });
+
+    const css1 = readFileSync(file1, 'utf8');
+    const css2 = readFileSync(file2, 'utf8');
+
+    /** @type {CombineCss.IOptions} */
+    const options = {
+        css1,
+        css2,
+    };
+
+    if (output) {
+        options.output = output;
+    }
+
+    if (parent) {
+        options.parent = parent;
+    }
+
+    return options;
+}
+
+function showHelp() {
+    console.log(`Usage:\n combine-unique-css file1.css file2.css [-o output.css]`);
+}
